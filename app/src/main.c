b987c7bee6d772be2008e5aa0a9bf72f6ca293a6
Brandon Mah <brandon.mah@ucalgary.ca>
2025-11-27 19:32:28 -0700

    Added some inline comments to main.c to make it more understandable.

--- app/src/main.c
+++ app/src/main.c
@@ -19,19 +19,23 @@
 int main(void) {
 
   if (0 > BTN_init()) {
-    return 0;
+    return 0;       // if init fails, just stop the program
   }
   if (0 > LED_init()) {
-    return 0;
+    return 0;       // if init fails, just stop the program
   }
-
+  //Initialize our state machine (sets initial state, clears counters, etc)
   state_machine_init();
-
+  //Main loop: repeatedly "tick" the state machine and sleep a bit
   while(1) {
-    int ret = state_machine_run();
-    if (0 > ret) {
+    int ret = state_machine_run();  //run one iteration of the FSM
+
+    if (0 > ret) {  // convention: non-zero or negative can mean "stop"
       return 0;
     }
+
+    // This below controls how often state_machine_run() is called.
+    // With SLEEP_MS = 1, we *attempt* to run it about once per millisecond.
     
     k_msleep(SLEEP_MS);
   }
