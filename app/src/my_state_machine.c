28ff956c92f2fc32116d138ed447c3717710f8dc
Brandon Mah <brandon.mah@ucalgary.ca>
2025-11-27 20:02:53 -0700

    Added Timing config, Button Mapping, and Function Prototypes for each state

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -1,21 +1,97 @@
 /**
  * @file my_state_machine.c
+ *
+ * This file implements the 5-state FMS from the diagram:
+ * 
+ * S0: all LEDs off
+ * S1: LED1 blinks at 4 Hz
+ * S2: LED1 & 3 on, LED2 & 4 off
+ * S3: LED1 & 3 off, LED2 & 4 on
+ * S4: all LEDs blink at 16 Hz
+ * 
  */
 
-#include <zephyr/smf.h>
+#include <zephyr/kernel.h)
+#include <zephyr/sys/printk.h>
+#include <zephyr/smf.h> // SMF types, macros, and functions
 
-#include "LED.h"
+#include "BTN.h"        // EiE button API
+#include "LED.h"        // EiE LED API
 #include "my_state_machine.h"
 
+/*-----------------------------------------------------------
+ * Timing Configuration
+ *-----------------------------------------------------------*/
+
+// Our main loop in main.c sleeps for SLEEP_MS_1, so each call to
+// state_machine_run() happens roughly every 1ms.
+#define TICK_MS 1u
+
+//How many "ticks" to get 1 second, assuming 1 ms per tick.
+#define TICKS_PER_SEC (1000u / TICK_MS)
 
 /*-----------------------------------------------------------
- * Function Prototypes
+ * Button Mapping
  *-----------------------------------------------------------*/
-static void led_on_state_exit(void* o);
-static enum smf_state_result led_on_state_run(void* o);
-static void led_off_state_exit(void* o);
-static enum smf_state_result led_off_state_run(void* o);
 
+/*
+* In the diagram they say "Button 1-4"
+* on the nRF board and EiE APIs we have BTN0-BTN3
+* We'll map them like this:
+*
+* Diagram Button 1 -> BTN0
+* Diagram Button 2 -> BTN1
+* Diagram Button 3 -> BTN2
+* Diagram Button 4 -> BTN3
+*/
+
+#define BUTTON1 BTN0
+#define BUTTON2 BTN1
+#define BUTTON3 BTN2
+#define BUTTON4 BTN3
+
+/*-----------------------------------------------------------
+ * Function Prototypes for each state's entry/run/exit
+ *
+ * SMF expects:
+ *  entry: void state_entry(void *obj)
+ *  run: enum smf_state_result state_run(void *obj)
+ *  exit: void state_exit(void *obj)
+ * 
+ * 'obj' is a pointer to *our* state machine object (defined later).
+ *-----------------------------------------------------------*/
+
+// S0: All LEDs off
+static void s0_entry(void *0);
+static enum smf_state_result s0_run(void *o);
+static void s0_exit(void *0);
+
+// S1: LED1 blink at 4 Hz
+static void s1_entry(void *o);
+static enum smf_state_result s1_run(void *o);
+static void s1_exit(void *o);
+
+// S2: LED1 & 3 on, LED2 & 4 off
+static void s2_entry(void *o);
+static enum smf_state_result s2_run(void *o);
+static void s2_exit(void *o);
+
+// S3: LED1 & 3 off, LED2 & 4 on
+static void s3_entry(void *o);
+static enum smf_state_result s3_run(void *o);
+static void s3_exit(void *o);
+
+// S4: All LEDs blink at 16 Hz
+static void s4_entry(void *o);
+static enum smf_state_result s4_run(void *o);
+static void s4_exit(void *o);
+
+/*-----------------------------------------------------------
+ * State IDs and state machine "object" type
+ *-----------------------------------------------------------*/
+
+
+ 
 /*-----------------------------------------------------------
  * Typedefs
  *-----------------------------------------------------------*/
