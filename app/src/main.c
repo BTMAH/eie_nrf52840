2f8ea554c2ababe4e090f0af7f0aa93fa807d4dc
Brandon Mah <brandon.mah@ucalgary.ca>
2026-03-09 13:37:30 -0600

    Added 4 buttons that do nothing

--- app/src/main.c
+++ app/src/main.c
@@ -36,9 +36,19 @@ int main(void) {
   if (0 > LED_init()) {
     return 0;
   }
+  for (uint8_t i = 0; i< NUM_LEDS; i++) {
+    lv_obj_t *ui_btn = lv_button_create(screen);
+    // Place the buttons in a 2x2 grid in the center of the screen
+    // matching the orientations of the LEDs on the board
+    lv_obj_align(ui_btn, LV_ALIGN_CENTER, 50 * (i % 2 ? 1 : -1), 20 * (i < 2 ? -1 : 1));
+    lv_obj_t *button_label = lv_label_create(ui_btn);
+    char label_text[10];
+    snprintf(label_text, 10, "LED %d", i);
+    lv_label_set_text(button_label, label_text);
+    lv_obj_align(button_label, LV_ALIGN_CENTER, 0, 0);
+  }
 
-  lv_obj_t *label = lv_label_create(screen);
-  lv_label_set_text(label, "Hello World!");
+ 
 
   display_blanking_off(display_dev);
   while (1) {
